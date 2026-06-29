4fe8fbf44f69c014362868347b0ccaa6a75da678
ptrawt <ptrawt@gmail.com>
2026-06-29 23:46:55 +0700

    fix: remove unused PayloadAction import, exclude sw.ts from app tsconfig

--- src/store/questsSlice.ts
+++ src/store/questsSlice.ts
@@ -1,4 +1,4 @@
-import { createSlice, createAsyncThunk, type PayloadAction } from '@reduxjs/toolkit'
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
 import { nanoid } from '@reduxjs/toolkit'
 import type { Quest, DailyCompletion } from '../types/quest'
 import {
